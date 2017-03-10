5f7dc0e5370a0fb8fd5b86b4fef6e40501418368
me-carl <c.almayda09@gmail.com>
2017-03-10 19:57:10 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import os, sys
+
+
+class Main():
+    __event_order_name = ''
+
+
+    def __init__():
+        self.__event_order_name = 'Hello World!'
+
+
+    def __event_label():
+        return self.__event_order_name
